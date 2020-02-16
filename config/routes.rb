76de01a45a6eed344c72e4979bca8b9e0d15b5a5
LibertyReman <libertyreman@gmail.com>
2020-02-16 15:37:57 +0900

    hello world

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  #ルート「/」へのGETリクエストが来た時、application_controller.rbコントローラの
+  #helloアクションにルーティングする
+  root 'application#hello'
 end
