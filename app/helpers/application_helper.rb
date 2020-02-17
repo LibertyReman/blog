a1cbcef889e66754ee0dcc97fc26c5786c8c921c
LibertyReman <libertyreman@gmail.com>
2020-02-18 01:49:40 +0900

    bootstrapを使用してヘッダーを作成

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,17 @@
 module ApplicationHelper
+  #_header.html.erbで使用するメソッドを定義 引数にリンク名とパスを指定
+  def header_link_item(name, path)
+    #bootstrapのデザインに合わせるために必要なクラス
+    class_name = 'nav-item'
+    #current_pageで表示するパスと引数のパスが同じかを判断している
+    #同じだった場合クラスの指定にactivwを追加する
+    class_name << ' active' if current_page?(path)
+
+    #content_tagは任意のHTMLを作るためのヘルパーメソッド :liタグを作成しクラスには先ほど作ったクラス名を
+    #指定している
+    #liタグの中にlink_toのaタグが埋め込まれる　ヘルパーの呼び出し元に返される
+    content_tag :li, class: class_name do
+      link_to name, path, class: 'nav-link'
+    end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,8 @@ Rails.application.routes.draw do
   #ルート「/」へのGETリクエストをhome_controller.rbコントローラのindexアクションにルーティング
   root 'home#index'
 
+  #/aboutのURLでhome_controller.rbコントローラのaboutアクションにルーティング
+  #about_pathやabout_urlといった名前付きルートの使用が可能
+  get  '/about',   to: 'home#about'
+
 end
