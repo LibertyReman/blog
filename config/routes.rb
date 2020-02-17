40b47e4764993058c673683f24701b63d24589c2
LibertyReman <libertyreman@gmail.com>
2020-02-17 19:52:33 +0900

    ホームページを追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,5 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  #ルート「/」へのGETリクエストをhome_controller.rbコントローラのindexアクションにルーティング
+  root 'home#index'
 
-  #ルート「/」へのGETリクエストが来た時、application_controller.rbコントローラの
-  #helloアクションにルーティングする
-  root 'application#hello'
 end
