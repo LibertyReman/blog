b751adda7bd7d2593ded4cda1147d50ca9f12e74
LibertyReman <libertyreman@gmail.com>
2020-02-23 00:18:02 +0900

    ナビゲーションバーの文字の縦配置のズレを修正

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,7 +2,7 @@ module ApplicationHelper
   #_header.html.erbで使用するメソッドを定義 引数にリンク名とパスを指定
   def header_link_item(name, path)
     #bootstrapで必要するクラスを代入
-    class_name = 'nav-item mr-1 h5'
+    class_name = 'nav-item mr-1 h5 my-0'
     #current_pageで表示するパスと引数のパスが同じかを判断している
     #同じだった場合クラスの指定にactivwを追加する
     class_name << ' active' if current_page?(path)
