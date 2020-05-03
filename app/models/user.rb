53d42483f8dfb37a7fe601a5af65b7b906fe0335
LibertyReman <libertyreman@gmail.com>
2020-05-03 11:14:30 +0900

    バリデーションと削除時の関係性を追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,6 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  #ユーザが消えたラプフィールも消す
+  has_one :profile, dependent: :destroy
 end
