0aadcab9576f223818c0bc492820ffcdaf2d552f
LibertyReman <libertyreman@gmail.com>
2020-05-04 08:34:41 +0900

    devise機能のサインアップ・パスワード再設定のURLにアクセスできないようにする

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,5 @@
 class User < ApplicationRecord
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
-  #ユーザが消えたラプフィールも消す
-  has_one :profile, dependent: :destroy
+  #devise機能のサインアップ・パスワード再設定のURLにアクセスできないようにする
+  devise :database_authenticatable,
+         :rememberable, :validatable
 end
