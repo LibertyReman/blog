10b4079ca4a7021b457a02be996cb0fdf193babe
LibertyReman <libertyreman@gmail.com>
2020-05-06 15:56:48 +0900

    DBにカテゴリーを作成するためにseeds.rbを編集。その後、$ rails db:seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,15 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+# DBにカテゴリーデータを作成する
+Tag.create([
+  { name: 'Technology' },
+  { name: 'Business' },
+  { name: 'Lifehack' },
+  { name: 'Diary' }
+])
+
+
+
+
