0b5a53491a2ad50ffdb15988eb4983e6aba035a6
LibertyReman <libertyreman@gmail.com>
2020-05-06 11:48:15 +0900

    タグと記事のデータベースの関連付けを設定

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,4 +1,9 @@
 class Article < ApplicationRecord
+  #一つの記事が複数のarticle_tag_relationsデータを持つ
+  has_many :article_tag_relations
+  #一つの記事が複数のtagsを持つ throughでarticle_tag_relationsと関連付け
+  has_many :tags, through: :article_tag_relations
+
   validates :title, presence: true
   validates :content, presence: true
   validates :image, presence: true
--- app/models/tag.rb
+++ app/models/tag.rb
@@ -1,2 +1,6 @@
 class Tag < ApplicationRecord
+  #一つのタグが複数のarticle_tag_relationsデータを持つ
+  has_many :article_tag_relations
+  #一つのタグが複数のarticlesを持つ throughでarticle_tag_relationsと関連付け
+  has_many :articles, through: :article_tag_relations
 end
