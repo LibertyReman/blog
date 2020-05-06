1f2ef7ed352ad1af7eeb158ca8cdedda08179d78
LibertyReman <libertyreman@gmail.com>
2020-05-06 11:58:34 +0900

    タグや記事の削除時に関連付けも削除するよう設定

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,6 +1,7 @@
 class Article < ApplicationRecord
   #一つの記事が複数のarticle_tag_relationsデータを持つ
-  has_many :article_tag_relations
+  #削除時に関連するタグのヒモ付を削除
+  has_many :article_tag_relations, dependent: :delete_all
   #一つの記事が複数のtagsを持つ throughでarticle_tag_relationsと関連付け
   has_many :tags, through: :article_tag_relations
 
--- app/models/tag.rb
+++ app/models/tag.rb
@@ -1,6 +1,7 @@
 class Tag < ApplicationRecord
   #一つのタグが複数のarticle_tag_relationsデータを持つ
-  has_many :article_tag_relations
+  #削除時に関連するタグのヒモ付を削除
+  has_many :article_tag_relations, dependent: :delete_all
   #一つのタグが複数のarticlesを持つ throughでarticle_tag_relationsと関連付け
   has_many :articles, through: :article_tag_relations
 end
