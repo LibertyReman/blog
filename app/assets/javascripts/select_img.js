619adcbf05ac53c71ab8c4e1b74767bf11a74439
LibertyReman <libertyreman@gmail.com>
2020-05-14 23:37:34 +0900

    ファイラーで選択された写真をプレビューする機能を追加

--- app/assets/javascripts/select_img.js
+++ app/assets/javascripts/select_img.js
@@ -1,16 +1,22 @@
 $(function() {
+    //.cover-imgのクリックイベントを登録
     $(".cover-img").on('click', function(){
-        $("input[type='file']").on('click', function(e){
-            e.stopPropagation();
-        });
         $("input[type='file']").click();
-        $("input[type='file']").on('change',function(){
-        });
     });
-    return
+
+    //クリックされたらファイラーを開く
+    $("input[type='file']").on('click', function(e){
+        e.stopPropagation();
+    });
+
+    //画像更新の処理を追加する
+    $("input[type='file']").on('change',function(e){
+        //console.log($(this))
+        var reader = new FileReader();
+        reader.onload = function (e) {
+            $("#display-img").attr('src', e.target.result);
+        }
+        reader.readAsDataURL(e.target.files[0]);
+    });
 });
-
-
-
-
 
