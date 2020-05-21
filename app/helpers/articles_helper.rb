ce0dd0308e0b1b5d10f06837f5d5f8f2a430925e
LibertyReman <libertyreman@gmail.com>
2020-05-21 09:14:58 +0900

    redcarpetを使用してマークダウン文章をHTMLに変換して表示する

--- app/helpers/articles_helper.rb
+++ app/helpers/articles_helper.rb
@@ -1,2 +1,24 @@
 module ArticlesHelper
+
+  def md_to_html(text)
+    options = {
+      filter_html: false,
+      hard_wrap: true,
+      space_after_headers: true
+    }
+
+    extensions = {
+      autolink: true,
+      no_intra_emphasis: true,
+      fenced_code_blocks: true,
+      space_after_headers: true,
+      highlight: true,
+      tables: true
+    }
+
+    renderer = Redcarpet::Render::HTML.new(options)
+    markdown = Redcarpet::Markdown.new(renderer, extensions)
+
+    markdown.render(text).html_safe
+  end
 end
