4df8e8b6106bb9bad0a18cf5aeb78bec01c452a9
LibertyReman <libertyreman@gmail.com>
2020-05-23 14:01:51 +0900

    指定した範囲の画像が表示されない

--- app/models/article.rb
+++ app/models/article.rb
@@ -5,10 +5,6 @@ class Article < ApplicationRecord
   #一つの記事が複数のtagsを持つ throughでarticle_tag_relationsと関連付け
   has_many :tags, through: :article_tag_relations
 
-  validates :title, presence: true
-  validates :content, presence: true
-  validates :image, presence: true
-  validates :tag_ids, presence: true
 
   attachment :image
 end
