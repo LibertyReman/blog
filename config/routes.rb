54e6d064814bec4de945ce0fa54614c53ad4a5a3
LibertyReman <libertyreman@gmail.com>
2020-05-29 05:01:08 +0900

    記事一覧のタイトルの表示エリアを拡大

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  devise_for :users, path: '', path_names: { sign_in: 'edit/login', sign_out: 'logout'}
+  devise_for :users, path: '', path_names: {sign_in: 'edit/login'}
   resources :articles
   #ルート「/」へのGETリクエストをhome_controller.rbコントローラのindexアクションにルーティング
   root 'articles#index'
