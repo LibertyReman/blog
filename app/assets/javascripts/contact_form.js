e834b6b3b4eb427d752c2afb6e05a272555fadec
LibertyReman <libertyreman@gmail.com>
2020-05-29 21:36:41 +0900

    微妙なスマフォでのレイアウト修正

--- app/assets/javascripts/contact_form.js
+++ app/assets/javascripts/contact_form.js
@@ -7,7 +7,7 @@ window.onload = function() {
         address1 = 'libertyreman';
         address2 = '@gmail.com';
         address = address1+address2;
-        subject = 'お問い合わせ';
+        subject = 'ブログからのお問い合わせ';
         body = '【名前】' + '%0D%0A' + nameData + '%0D%0A' + '【メッセージ】' + '%0D%0A' + msgData;
         location.href = 'mailto:' + address + '?subject=' + subject + '&body=' + body;
     };
